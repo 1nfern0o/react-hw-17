6721f8035c9dc54d1a532d400e2da36486d1178e
Roman <romantcvetkov@gmail.com>
2020-09-28 14:30:51 +0300

    add hw 17

---
+++ src/Components/Timer/timers.js
@@ -0,0 +1,40 @@
+import React, {useEffect, useState} from "react";
+import "./timer.css";
+/*import Timer from "./timer";*/
+
+const Timers = ({autoStart= false, startTime = 0, speedTime = "1000"}) => {
+
+    const [time, setTime] = useState(startTime);
+    const [isActive, setIsActive] = useState(autoStart);
+
+    const timeToggle = () => {
+        setIsActive(!isActive);
+    }
+
+    useEffect(() => {
+        let interval = null;
+        if (isActive && time > 0) {
+            interval = setInterval(() => {
+                setTime((time) => time - 1);
+            }, speedTime);
+        } else if (!isActive && time !== 0) {
+            clearInterval(interval)
+        }
+        return() => clearInterval(interval);
+    }, [isActive, time, startTime])
+
+    return (
+        <div className="timer-wrapper">
+            <div className="timer-block">
+                <h3>Speed time: {speedTime} ms</h3>
+                <button className="timer-btn" onClick={timeToggle}>
+                    {isActive ? "Pause" : "Start"}
+                </button>
+                <h2>{time}</h2>
+            </div>
+            <div className="timer-line" style={{ width: `${time}%`}}></div>
+        </div>
+    )
+}
+
+export default Timers;
